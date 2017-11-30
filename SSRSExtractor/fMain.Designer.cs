a1a32639b3f81e5e983b0c2c6a5044a8e4854ccc
fabiano <fabianoconrado@gmail.com>
2017-11-30 13:24:15 -0200

    Incluindo arquivos do projeto

---
+++ SSRSExtractor/fMain.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SSRSExtractor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbLocations = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(135, 164);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStatus.Location = new System.Drawing.Point(12, 133);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(81, 13);
+            this.lbStatus.TabIndex = 12;
+            this.lbStatus.Text = "Processing...";
+            this.lbStatus.Visible = false;
+            // 
+            // progBar
+            // 
+            this.progBar.Location = new System.Drawing.Point(1, 202);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(334, 23);
+            this.progBar.TabIndex = 11;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(15, 100);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(307, 20);
+            this.txtOutput.TabIndex = 10;
+            this.txtOutput.Text = "C:\\Users\\fabiano.conrado\\Documents\\Visual Studio 2015\\Reports";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Output to:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "SSRS Location";
+            // 
+            // cmbLocations
+            // 
+            this.cmbLocations.FormattingEnabled = true;
+            this.cmbLocations.Location = new System.Drawing.Point(15, 54);
+            this.cmbLocations.Name = "cmbLocations";
+            this.cmbLocations.Size = new System.Drawing.Size(307, 21);
+            this.cmbLocations.TabIndex = 7;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 226);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.progBar);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbLocations);
+            this.Controls.Add(this.btnGo);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SSRS Extractor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.ProgressBar progBar;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbLocations;
+    }
+}
+
---
+++ SSRSExtractor/fMain.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Web.Services.Protocols;
+using System.Xml;
+using System.Net;
+using System.IO;
+
+namespace SSRSExtractor
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+
+            GetLocations();
+
+            cmbLocations.SelectedIndex = 0;
+        }
+
+        private void GetLocations()
+        {
+            RSWebReference_SSRS.ReportingService2010 rs = new RSWebReference_SSRS.ReportingService2010();
+            rs.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            try
+            {
+                cmbLocations.Items.Add("/");
+
+                RSWebReference_SSRS.CatalogItem[] items = rs.ListChildren(@"/", false);
+                foreach (RSWebReference_SSRS.CatalogItem item in items)
+                {
+                    if (item.TypeName == "Folder")
+                        cmbLocations.Items.Add(item.Name);
+                }
+            }
+            catch (SoapException)
+            {
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            lbStatus.Text = "Processing...";
+            lbStatus.Visible = true;
+
+            Application.DoEvents();
+
+            string sFrom = cmbLocations.Text;
+            //string sOutputDir = string.Format(@"{0}{1}\", txtOutput.Text, sFrom);
+            string sOutputDir = string.Format(@"{0}", txtOutput.Text);
+            string sOutFile = "";
+
+            if (!Directory.Exists(sOutputDir))
+                Directory.CreateDirectory(sOutputDir);
+
+            RSWebReference_SSRS.ReportingService2010 rs = new RSWebReference_SSRS.ReportingService2010();
+            rs.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            try
+            {
+                //RSWebReference_SSRS.CatalogItem[] items = rs.ListChildren(string.Format(@"/{0}", sFrom), true);
+                RSWebReference_SSRS.CatalogItem[] items = rs.ListChildren(@"/", true);
+
+                progBar.Maximum = items.Count();
+                progBar.Value = 0;
+
+                foreach (RSWebReference_SSRS.CatalogItem item in items)
+                {
+                    if (item.TypeName == "Report" ) {
+                        byte[] rpt_def = null;
+                        XmlDocument doc = new XmlDocument();
+
+                        rpt_def = rs.GetItemDefinition(item.Path);
+                        MemoryStream stream = new MemoryStream(rpt_def);
+
+                        sOutFile = string.Format(@"{0}{1}.rdl", sOutputDir, item.Path.Replace("/","\\"));
+
+                        if (File.Exists(sOutFile))
+                            File.Delete(sOutFile);
+
+                        doc.Load(stream);
+                        doc.Save(sOutFile);
+                    }
+                    else if(item.TypeName == "Folder")
+                    {
+                        var path = string.Format(@"{0}{1}", sOutputDir, item.Path.Replace("/", "\\"));
+                        if (!Directory.Exists(path))
+                            Directory.CreateDirectory(path);
+                    }
+
+                    progBar.Value += 1;
+                }
+            }
+            catch (SoapException)
+            {
+            }
+
+            lbStatus.Text = "Complete";
+            progBar.Value = progBar.Maximum;
+        }
+    }
+}
+
+
